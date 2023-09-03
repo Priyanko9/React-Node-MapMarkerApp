e8c193a0e2edf3829282e09690069643fb1ad80d
Priyanko Das <pridas2@WKMZT0210FEC.global.publicisgroupe.net>
2023-09-03 19:46:51 +0530

    push build folder

---
+++ client/build/precache-manifest.0a79b969e3bf5a8f958c24d92e80bfba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d285009d36c803c46301cd48ae9d0ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4a040455405f0a74d8",
+    "url": "/static/css/main.5d1f34d6.chunk.css"
+  },
+  {
+    "revision": "6c009699502790d2bd55",
+    "url": "/static/js/2.b043dda3.chunk.js"
+  },
+  {
+    "revision": "fc4a040455405f0a74d8",
+    "url": "/static/js/main.514141db.chunk.js"
+  },
+  {
+    "revision": "0021d159b8343cfaf18e",
+    "url": "/static/js/runtime~main.d49aaa73.js"
+  }
+]);
