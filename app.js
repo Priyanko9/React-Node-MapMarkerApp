81510eada9895138ab5d33573fdee2502616ea17
Priyanko Das <pridas2@WKMZT0210FEC.global.publicisgroupe.net>
2023-09-03 19:55:28 +0530

    serve client from server

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const markerRouter=require('./routes/markerRoutes');
-const localStorage=require('localStorage');
-localStorage.setItem("test","ywi");
-const app=express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const markerRouter = require("./routes/markerRoutes");
+const localStorage = require("localStorage");
+localStorage.setItem("test", "ywi");
+const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/marker',markerRouter);
+app.use("/marker", markerRouter);
+app.use(express.static("./client/build/"));
 
 app.listen(3006);
-
